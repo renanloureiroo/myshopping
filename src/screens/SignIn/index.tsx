95f3df95154fc78d03c1827bec206fbc2df888df
Renan Loureiro <renanpl47@gmail.com>
2022-02-08 18:37:51 -0300

    feat: account creation with email and password

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from "react"
+import auth from "@react-native-firebase/auth"
 
-import { Container, Account, Title, Subtitle } from './styles';
-import { ButtonText } from '../../components/ButtonText';
-import { Button } from '../../components/Button';
-import { Input } from '../../components/Input';
+import { Container, Account, Title, Subtitle } from "./styles"
+import { ButtonText } from "../../components/ButtonText"
+import { Button } from "../../components/Button"
+import { Input } from "../../components/Input"
+import { Alert } from "react-native"
 
 export function SignIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  async function handleCreateUser() {
+    try {
+      await auth().createUserWithEmailAndPassword(email, password)
+      Alert.alert("Usuário criado com sucesso!")
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async function handleSignIn() {
+    try {
+      await auth().signInWithEmailAndPassword(email, password)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <Container>
       <Title>MyShopping</Title>
@@ -14,19 +36,23 @@ export function SignIn() {
       <Input
         placeholder="e-mail"
         keyboardType="email-address"
+        onChangeText={setEmail}
+        value={email}
       />
 
       <Input
         placeholder="senha"
         secureTextEntry
+        onChangeText={setPassword}
+        value={password}
       />
 
-      <Button title="Entrar" onPress={() => { }} />
+      <Button title="Entrar" onPress={handleSignIn} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Recuperar senha" onPress={() => {}} />
+        <ButtonText title="Criar minha conta" onPress={handleCreateUser} />
       </Account>
     </Container>
-  );
+  )
 }
